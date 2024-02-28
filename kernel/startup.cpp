ab309ebaa5aec3bfdf432726dee7263b9ecf1d75
hgruniaux <hubert@gruniaux.com>
2024-02-28 17:01:24 +0100

    [fix] Cleanup startup code and move more things to C++

---
+++ kernel/startup.cpp
@@ -0,0 +1,74 @@
+/*
+ * crti.cxx for ARM - BPABI
+ *
+ * Implements ARM-specific code to invoke C++ global constructors on the ARM
+ * platform, ensuring proper initialization of global and static objects before
+ * program execution.
+ */
+
+#include <cstdint>
+
+// The following pointers are provided by the linker.
+extern uint32_t __bss_start;
+extern uint32_t __bss_end;
+
+/** Erases the BSS section. */
+void
+zero_bss()
+{
+  uint32_t* dst = &__bss_start;
+  while (dst < &__bss_end) {
+    *dst++ = 0;
+  }
+}
+
+using FunctionPointer = void (*)();
+
+// The following pointers are provided by the linker.
+extern FunctionPointer __init_array_start[];
+extern FunctionPointer __init_array_end[];
+extern FunctionPointer __fini_array_start[];
+extern FunctionPointer __fini_array_end[];
+
+/** Calls all functions registered in the .init_array section.
+ * This section is populated by the C and C++ compiler to implement global
+ * constructors. */
+void
+call_init_array()
+{
+  // Call the constructors.
+  auto array = __init_array_start;
+  while (array < __init_array_end) {
+    (*array)();
+    array++;
+  }
+}
+
+/** Calls all functions registered in the .fini_array section.
+ * This section is populated by the C and C++ compiler to implement global
+ * destructors. */
+void
+call_fini_array()
+{
+  // Call the destructors.
+  auto array = __fini_array_start;
+  while (array < __fini_array_end) {
+    (*array)();
+    array++;
+  }
+}
+
+extern "C" void
+kmain();
+
+/** The C and C++ world entry point. It is called from the boot.S assembly script. */
+extern "C" void
+_startup()
+{
+  // Erases the BSS section as required.
+  zero_bss();
+
+  call_init_array();
+  kmain(); // the real kernel entry point
+  call_fini_array();
+}
