37c5b73c08890b323834260da2b7e25d660ee349
Hubert Gruniaux <hubert@gruniaux.com>
2024-03-04 19:48:39 +0100

    [feat] Factorize out UART, MMIO and Mailbox interfaces (#17)
    * [feat] Factorize out UART, MMIO and Mailbox interfaces
    
    * [fix] Update CMakeLists.txt with missing files

---
+++ kernel/mailbox.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdint>
+
+namespace MailBox {
+/** The different supported mailbox channels by our kernel. */
+enum class Channel : uint32_t {
+  /** Property channel ARM CPU to VideoCore. */
+  TagArmToVC = 8,
+  /** Property channel VideoCore to ARM CPU. */
+  TagVCToArm = 9,
+};  // enum class Channel
+
+/** Receives a 28-bit raw message from mailbox in the requested @a channel. */
+uint32_t receive(Channel channel);
+/** Sends a 28-bit @a message to mailbox in the requested @a channel. */
+void send(Channel channel, uint32_t message);
+}  // namespace MailBox
