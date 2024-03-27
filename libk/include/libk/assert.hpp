b66fec9ea35827a8d61eb1c75c0a6007e33fe2f7
hgruniaux <hubert@gruniaux.com>
2024-03-27 15:57:31 +0100

    Improvements on the libk

--- libk/include/libk/assert.hpp
+++ libk/include/libk/assert.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <source_location>
+
+namespace debug {
+[[noreturn]] void panic(const char* message, std::source_location source_location = std::source_location::current());
+}  // namespace debug
+
 #ifndef KASSERT
-#define KASSERT(cond)
-#endif // KASSERT
+#define KASSERT(cond) (void)((cond) || (::debug::panic("assertion failed: " #cond), 0))
+#endif  // KASSERT
--- libk/src/string.cpp
+++ libk/src/string.cpp
@@ -3,16 +3,20 @@
 #if __ARM_FEATURE_SVE
 extern "C" size_t __strlen_aarch64_sve(const char*);
 extern "C" char* __strcpy_aarch64_sve(char*, const char*);
+extern "C" void* __memchr_aarch64_sve(const void*, int, size_t);
+extern "C" int __memcmp_aarch64_sve(const void*, const void*, size_t);
 extern "C" void* __memcpy_aarch64_sve(void*, const void*, size_t);
 #else
 extern "C" size_t __strlen_aarch64(const char*);
 extern "C" char* __strcpy_aarch64(char*, const char*);
+extern "C" void* __memchr_aarch64(const void*, int, size_t);
+extern "C" int __memcmp_aarch64(const void*, const void*, size_t);
 extern "C" void* __memcpy_aarch64(void*, const void*, size_t);
 #endif
 extern "C" void* __memset_aarch64(void*, int, size_t);
+extern "C" void* __memmove_aarch64_mops(void*, const void*, size_t);
 
 namespace libk {
-namespace detail {
 size_t strlen(const char* text) {
 #if __ARM_FEATURE_SVE
   return __strlen_aarch64_sve(text);
@@ -20,13 +24,28 @@ size_t strlen(const char* text) {
   return __strlen_aarch64(text);
 #endif
 }
-}  // namespace detail
 
 char* strcpy(char* dst, const char* src) {
 #if __ARM_FEATURE_SVE
   return __strcpy_aarch64_sve(dst, src);
 #else
   return __strcpy_aarch64(dst, src);
+#endif
+}
+
+void* memchr(const void* ptr, int value, size_t length) {
+#if __ARM_FEATURE_SVE
+  return __memchr_aarch64_sve(ptr, value, length);
+#else
+  return __memchr_aarch64(ptr, value, length);
+#endif
+}
+
+int memcmp(const void* lhs, const void* rhs, size_t length) {
+#if __ARM_FEATURE_SVE
+  return __memcmp_aarch64_sve(lhs, rhs, length);
+#else
+  return __memcmp_aarch64(lhs, rhs, length);
 #endif
 }
 
@@ -41,15 +60,27 @@ void* memcpy(void* dst, const void* src, size_t length) {
   return __memcpy_aarch64(dst, src, length);
 #endif
 }
+
+void* memmove(void* dst, const void* src, size_t length) {
+  return __memmove_aarch64_mops(dst, src, length);
+}
 }  // namespace libk
 
 #if LIBK_ENABLE_C
 extern "C" size_t strlen(const char* text) {
-  return libk::detail::strlen(text);
+  return libk::strlen(text);
 }
 
 extern "C" char* strcpy(char* dst, const char* src) {
   return libk::strcpy(dst, src);
+}
+
+extern "C" void* memchr(const void* ptr, int value, size_t length) {
+  return libk::memchr(ptr, value, length);
+}
+
+extern "C" int memcmp(const void* lhs, const void* rhs, size_t length) {
+  return libk::memcmp(lhs, rhs, length);
 }
 
 extern "C" void* memset(void* dst, int value, size_t length) {
@@ -58,5 +89,9 @@ extern "C" void* memset(void* dst, int value, size_t length) {
 
 extern "C" void* memcpy(void* dst, const void* src, size_t length) {
   return libk::memcpy(dst, src, length);
+}
+
+extern "C" void* memmove(void* dst, const void* src, size_t length) {
+  return libk::memmove(dst, src, length);
 }
 #endif  // LIBK_ENABLE_C
