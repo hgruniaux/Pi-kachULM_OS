f702318fcb955639a832599442c913f107e2038e
esauvat <emile.sauvat@proton.me>
2024-04-11 13:24:41 +0200

    bit array libk and page alloc

--- kernel/mmu/mmu_kernel.hpp
+++ kernel/mmu/mmu_kernel.hpp
@@ -1,56 +1,25 @@
-#ifndef MMU_KERNEL
-#define MMU_KERNEL
-
-#include "stdint.h"
+#pragma once
+#include <cstdint>
+#include "libk/bit_array.hpp"
 #define PAGESIZE 4096
 
-typedef void* physical_address;
+typedef void* physical_address_t;
 
-class BitArray 
-{
+class PageAlloc {
+ public:
+  // Construcor
+  PageAlloc(uint64_t memsize);
+  // Utility functions
+  void setmmap(void* array);
+  void mark_as_used(physical_address_t addr);
+  bool freshpage(physical_address_t* addr);
+  void freepage(physical_address_t addr);
+  bool page_status(physical_address_t addr);
+  // Constant
+  const uint64_t memory_needed;
 
-    public:
-
-        // Constructeurs
-        explicit BitArray(void* begin, size_t bit_taille);
-        bool get_bit(size_t index) const;
-        void set_bit(size_t index, bool value);
-
-        // Opérateurs
-        bool operator[](size_t index) const
-        {
-            return get_bit(index);
-        }
-
-    private:
-
-        uint8_t* m_array;
-
+ protected:
+  libk::BitArray m_mmap;
+  const uint64_t m_memsize;
+  const uint64_t m_pagequant;
 };
-
-class PageAlloc
-{
-
-    public:
-
-        // Constructeur
-        PageAlloc(uint64_t memsize);
-
-        // Utility functions
-        void setmmap(void* array);
-        void mark_as_used(physical_address addr);
-        bool freshpage(physical_address* addr);
-        void freepage(physical_address addr);
-        bool page_status(physical_address addr);
-
-        const uint64_t memory_needed;
-        
-    protected:
-
-        BitArray mmap;
-        const uint64_t m_memsize;
-        const uint64_t m_pagequant;
-
-};
-
-#endif
