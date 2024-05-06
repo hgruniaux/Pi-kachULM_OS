ba75c5936f046df71d51cc64a08de7ef3c63b2ca
Hubert Gruniaux <hubert@gruniaux.com>
2024-05-06 22:12:46 +0200

    Add `assert` and `KASSERT_X` macros (#48)

--- lib/libk/include/libk/assert.hpp
+++ lib/libk/include/libk/assert.hpp
@@ -9,3 +9,12 @@ namespace libk {
 #ifndef KASSERT
 #define KASSERT(cond) (void)((cond) || (::libk::panic("assertion failed: " #cond, std::source_location::current()), 0))
 #endif  // KASSERT
+
+#ifndef KASSERT_X
+#define KASSERT_X(cond, msg) (void)((cond) || (::libk::panic("assertion failed: " msg, std::source_location::current()), 0))
+#endif // KASSERT_X
+
+#ifdef assert
+#undef assert
+#endif // assert
+#define assert(cond) KASSERT(cond)
