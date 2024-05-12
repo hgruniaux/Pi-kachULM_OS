4241b73acd502596df86d15948a8c6f9fb8e839f
hgruniaux <hubert@gruniaux.com>
2024-05-12 19:13:16 +0200

    Add libk::SharedPointer

--- lib/libk/include/libk/memory.hpp
+++ lib/libk/include/libk/memory.hpp
@@ -1,16 +1,19 @@
 #pragma once
 
+#include <atomic>
+#include <cstddef>
+
 #include "assert.hpp"
 
 namespace libk {
 template <class T>
 class ScopedPointer {
  public:
-  ScopedPointer(T* data = nullptr) : m_data(data) {}
-  ~ScopedPointer() {
-    if (m_data != nullptr)
-      delete m_data;
-  }
+  ScopedPointer() : m_data(nullptr) {}
+  ScopedPointer(std::nullptr_t) : m_data(nullptr) {}
+  template <class U>
+  explicit ScopedPointer(U* data = nullptr) : m_data(data) {}
+  ~ScopedPointer() { reset(); }
 
   // No copy
   ScopedPointer(const ScopedPointer&) = delete;
@@ -24,6 +27,18 @@ class ScopedPointer {
     return *this;
   }
 
+  void reset() {
+    if (m_data != nullptr)
+      delete m_data;
+    m_data = nullptr;
+  }
+
+  template <class U>
+  void reset(U* ptr) {
+    reset();
+    m_data = ptr;
+  }
+
   [[nodiscard]] T* get() const { return m_data; }
   [[nodiscard]] operator bool() const { return m_data != nullptr; }
   [[nodiscard]] bool operator!() const { return m_data == nullptr; }
@@ -33,4 +48,72 @@ class ScopedPointer {
  private:
   T* m_data;
 };  // class ScopedPointer
+
+template <class T>
+class SharedPointer {
+ public:
+  SharedPointer() : m_block(nullptr) {}
+  SharedPointer(std::nullptr_t) : m_block(nullptr) {}
+  template <class U>
+  explicit SharedPointer(U* ptr) : m_block(nullptr) {
+    reset(ptr);
+  }
+  ~SharedPointer() { reset(); }
+
+  // Copy
+  SharedPointer(const SharedPointer& other) : m_block(other.m_block) {
+    if (m_block != nullptr)
+      m_block->ref_count++;
+  }
+  SharedPointer& operator=(const SharedPointer& other) {
+    if (m_block == other.m_block)  // check self-assignment
+      return *this;
+
+    reset();
+    new (this) SharedPointer(other);
+    return *this;
+  }
+
+  // Move
+  SharedPointer(SharedPointer&& other) : m_block(other.m_block) { other.m_block = nullptr; }
+  SharedPointer& operator=(SharedPointer&& other) {
+    reset();
+    m_block = other.m_block;
+    other.m_block = nullptr;
+    return *this;
+  }
+
+  void reset() {
+    if (m_block == nullptr)
+      return;
+
+    m_block->ref_count--;
+    if (m_block->ref_count == 0) {
+      delete m_block->data;
+      delete m_block;
+    }
+  }
+
+  template <class U>
+  void reset(U* ptr) {
+    reset();
+    m_block = new Block;
+    m_block->data = ptr;
+    m_block->ref_count++;
+  }
+
+  [[nodiscard]] T* get() const { return m_block->data; }
+  [[nodiscard]] operator bool() const { return m_block->data != nullptr; }
+  [[nodiscard]] bool operator!() const { return m_block->data == nullptr; }
+  [[nodiscard]] T& operator*() const { return *m_block->data; }
+  [[nodiscard]] T* operator->() const { return m_block->data; }
+
+ private:
+  struct Block {
+    T* data = nullptr;
+    unsigned int ref_count = 0;
+  };  // struct Block
+
+  Block* m_block;
+};  // class SharedPointer
 }  // namespace libk
