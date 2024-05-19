15f21d80d1b396888975747268544e9f3fb4c102
Gabriel Desfrene <gabriel@desfrene.fr>
2024-05-19 15:26:19 +0200

    Zeroing pages on allocation, and freeing them on Memory Chunck destruction.

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -19,60 +19,6 @@
 #define COMPILER_NAME "Unknown Compiler"
 #endif
 
-static inline constexpr uint32_t CHRONO_MS_PERIOD = 0x100;
-
-uint16_t ms = 0;
-uint8_t s = 0;
-uint64_t m = 0;
-
-template <class T>
-void int_to_hex_string(char* buffer, T value) {
-  static const char hex_digits[] = "0123456789ABCDEF";
-
-  for (size_t i = 0; i < sizeof(T) * 2; ++i) {
-    buffer[i] = hex_digits[(value >> (4 * (sizeof(T) * 2 - (i + 1)))) & 0xf];
-  }
-
-  buffer[sizeof(T) * 2] = 0;
-}
-
-void update_timer() {
-  ms += CHRONO_MS_PERIOD;
-  if (ms >= 1000) {
-    s += ms / 1000;
-    ms %= 1000;
-
-    if (s >= 60) {
-      m += s / 60;
-      s %= 60;
-    }
-  }
-}
-
-void draw_timer() {
-  static char buffer[sizeof(uint64_t) * 4 + 1] = {0};
-  graphics::Painter painter;
-
-  painter.set_pen(graphics::Color::WHITE);
-  painter.fill_rect(50, 50, 400, 100);
-  painter.set_pen(graphics::Color::RED);
-  painter.draw_rect(50, 50, 400, 100);
-
-  // Draw the text at the middle of screen
-  painter.set_pen(graphics::Color::BLACK);
-
-  painter.draw_text(50, 50, "Hex Clock: ");
-
-  int_to_hex_string(buffer, m);
-  painter.draw_text(50, 70, buffer);
-
-  int_to_hex_string(buffer, s);
-  painter.draw_text(250, 70, buffer);
-
-  int_to_hex_string(buffer, ms);
-  painter.draw_text(300, 70, buffer);
-}
-
 extern "C" const char init[];
 
 [[noreturn]] void kmain() {
