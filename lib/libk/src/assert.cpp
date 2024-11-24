34a41eb0381ec0056d27cf93bbe7fad12fa4e213
hgruniaux <hubert@gruniaux.com>
2024-11-24 12:35:33 +0100

    Try to exit qemu when panicking

--- lib/libk/src/assert.cpp
+++ lib/libk/src/assert.cpp
@@ -1,12 +1,18 @@
 #include <libk/log.hpp>
 #include <libk/utils.hpp>
 
+#include <libk/qemu.hpp>
+
 namespace libk {
 [[noreturn]] void panic(const char* message, std::source_location source_location) {
   print("KERNEL PANIC at {}:{} in `{}`.", source_location.file_name(), source_location.line(),
         source_location.function_name());
   print("Do not panic. Keep calm and carry on.");
   print("Panic message: {}", message);
+
+#ifdef TARGET_QEMU
+    qemu_exit(1);
+#endif // TARGET_QEMU
 
   // Enter an infinite loop, so we don't return from this function.
   libk::halt();
